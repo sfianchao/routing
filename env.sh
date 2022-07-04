e9eae6be528ff10b14edc881208c02f0314ad4b5
sfianchao <30313481+sfianchao@users.noreply.github.com>
2022-07-04 18:14:31 +0800

    fix

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 set -e
 
 # download & install java 8
-echo "step 1. download & install java 8\n"
+echo "\nStep 1. download & install java 8\n"
 sudo apt install openjdk-8-jdk
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=$PATH:$JAVA_HOME/bin
@@ -10,14 +10,14 @@ sudo java -version
 sudo javac -version
 
 # download & install solc
-echo "step 2. download & install solidity compiler\n"
+echo "\nStep 2. download & install solidity compiler\n"
 sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt-get update
 sudo apt-get install solc
 sudo solc --version
 
 # download & install web3j
-echo "step 3. download & install web3j\n"
+echo "\nStep 3. download & install web3j\n"
 sudo curl -L get.web3j.io | sh
-source $HOME/.web3j/source.sh
-sudo web3j -version
+sudo bash $HOME/.web3j/source.sh
+web3j -version
--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 set -e
 
 # setup smart contract
-echo "step 1. compile solidity file\n"
+echo "\nStep 1. compile solidity file\n"
 sudo solc Proxy.sol --bin --abi --optimize --overwrite -o ./
 
-echo "step 2. web3j convert to java class\n"
-sudo web3j generate solidity -b ./Proxy.bin -a ./Proxy.abi -o ./src/main/java -p idsl.crosschain.routing.contract
+echo "\nStep 2. web3j convert to java class\n"
+web3j generate solidity -b ./Proxy.bin -a ./Proxy.abi -o ./src/main/java -p idsl.crosschain.routing.contract
 
