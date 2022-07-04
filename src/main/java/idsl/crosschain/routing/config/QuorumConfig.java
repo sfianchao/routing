70fc5ebb9641d9bebcc11e949554548f0165945a
sfianchao <30313481+sfianchao@users.noreply.github.com>
2022-07-04 16:03:11 +0800

    idsl prod
    chain ip

--- src/main/java/idsl/crosschain/routing/config/QuorumConfig.java
+++ src/main/java/idsl/crosschain/routing/config/QuorumConfig.java
@@ -2,6 +2,7 @@ package idsl.crosschain.routing.config;
 
 import idsl.crosschain.routing.model.QuorumInfo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.web3j.crypto.Credentials;
@@ -16,9 +17,8 @@ import java.math.BigInteger;
 @Configuration
 public class QuorumConfig {
 
-    private String idslInternalIp = "http://192.168.66.74:8545";
-    private String idslExternalIp = "http://140.118.9.214:9045";
-    private String masterPlanIp = "http://140.118.9.226:23001";
+    @Value("${quorum.chain.relay.ip}")
+    private String chainIp;
 
     @Bean(name = "relayChainBuilder")
     public QuorumInfo relayChainBuilder() {
@@ -27,7 +27,7 @@ public class QuorumConfig {
 
         try {
             // connect to blockchain
-            Quorum quorum = Quorum.build(new HttpService(masterPlanIp));
+            Quorum quorum = Quorum.build(new HttpService(chainIp));
             Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
             String clientVersion = web3ClientVersion.getWeb3ClientVersion();
             log.info("[relay] chain client version: " + clientVersion);
@@ -36,18 +36,6 @@ public class QuorumConfig {
             Credentials credentials = Credentials.create("ba394d6ecde04efb7bce0001f130d878794d890b9da8a27eb950a4b6aa58dd76");
             StaticGasProvider gasProvider = new StaticGasProvider(BigInteger.valueOf(20000000000L), BigInteger.valueOf(6721975L));
             log.info("[relay] chain account address: " + credentials.getAddress());
-
-            // connect to sfianchao relay chain
-//            Quorum quorum = Quorum.build(new HttpService("http://192.168.47.130:22000"));
-//            Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
-//            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
-//            log.info("[relay] chain client version: " + clientVersion);
-
-            // set up credentials
-//            Credentials credentials = WalletUtils.loadCredentials("node", new File("C:/Users/hmnic/Documents/GitHub/routing/wallets-relay/wallet1"));
-//            StaticGasProvider gasProvider = new StaticGasProvider(DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT);
-//            log.info("[relay] chain account address: " + credentials.getAddress());
-//            log.info("[relay} network version: {}", quorum.netVersion().send().getNetVersion());
 
             quorumInfo.setQuorum(quorum);
             quorumInfo.setCredentials(credentials);
