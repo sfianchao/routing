c5ae0bf9f40eb7b588162c052372e65db3440282
sfianchao <30313481+sfianchao@users.noreply.github.com>
2022-07-04 17:29:44 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,11 +9,20 @@ export PATH=$PATH:$JAVA_HOME/bin
 sudo java -version
 sudo javac -version
 
+# download & install solc
+echo "step 2. download & install solidity compiler"
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install solc
+sudo solc --version
+
 # download & install web3j
-echo "step 2. download & install web3j"
+echo "step 3. download & install web3j"
 sudo curl -L get.web3j.io | sh
 sudo web3j -version
 
 # setup smart contract
-echo "step 3. compile solidity file"
-sudo web3j generate solidity -b .\Proxy.bin -a .\Proxy.abi -o ./src/main/java -p idsl.crosschain.routing.contract
+echo "step 4. compile solidity file"
+sudo solc Proxy.sol --bin --abi --optimize --overwrite -o ./
+sudo web3j generate solidity -b ./Proxy.bin -a ./Proxy.abi -o ./src/main/java -p idsl.crosschain.routing.contract
+
